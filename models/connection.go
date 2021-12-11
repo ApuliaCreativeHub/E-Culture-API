303a1957751361a9a9cd5088b761fafd456705eb
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-11 12:15:22 +0100

    Create connection file in models

---
+++ models/connection.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"E-Culture-API/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+var Db *gorm.DB
+var configPath = "../conf/conf.yml"
+
+func init() {
+	err := utils.ValidateConfigPath(configPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conf, err := utils.NewConfig(configPath)
+	dsn := conf.Database.User + ":" + conf.Database.Password + "@tcp(" + conf.Database.Host + ":" +
+		conf.Database.Port + ")/" + conf.Database.Schema + "?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NowFunc: func() time.Time {
+			return time.Now().Local()
+		},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
