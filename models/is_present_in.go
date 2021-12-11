ac583a9bf4d3e328cb27ec4b7948da48c946f60b
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-11 18:45:31 +0100

    Created AddObjectToPath

--- models/is_present_in.go
+++ models/is_present_in.go
@@ -5,3 +5,12 @@ type IsPresentIn struct {
 	PathId   uint `gorm:"primaryKey"`
 	Order    uint `gorm:"primaryKey"`
 }
+
+func (IsPresentIn) TableName() string {
+	return "is_present_in"
+}
+
+func AddObjectToPath(pathId, objectId, order uint) error {
+	tx := Db.Create(&IsPresentIn{PathId: pathId, ObjectId: objectId, Order: order})
+	return tx.Error
+}
