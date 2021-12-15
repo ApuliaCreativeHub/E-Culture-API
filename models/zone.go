4d3808156ab9b6e4ca76f3baa0cf4d21507cbdc5
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-15 12:42:58 +0100

    CRUD zone

--- models/zone.go
+++ models/zone.go
@@ -4,7 +4,7 @@ type Zone struct {
 	ID          uint
 	Name        string
 	Description string
-	PlaceId     uint
+	PlaceID     uint
 	Place       Place `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
@@ -12,8 +12,23 @@ func (Zone) TableName() string {
 	return "zone"
 }
 
-func CreateZone(name, description string, placeID uint) error {
-	zone := Zone{Name: name, Description: description, PlaceId: placeID}
-	tx := Db.Create(&zone)
+func (z *Zone) Create() error {
+	tx := Db.Create(z)
 	return tx.Error
+}
+
+func (z *Zone) Update() error {
+	tx := Db.Model(z).Updates(z)
+	return tx.Error
+}
+
+func (z *Zone) Delete() error {
+	tx := Db.Delete(z)
+	return tx.Error
+}
+
+func (z *Zone) ReadByPlaceId() ([]Zone, error) {
+	var zones []Zone
+	tx := Db.Where("place_id=?", z.PlaceID).Find(&zones)
+	return zones, tx.Error
 }
--- test/zone_test.go
+++ test/zone_test.go
@@ -1,13 +1,1 @@
 package test
-
-import (
-	"E-Culture-API/models"
-	"testing"
-)
-
-func TestCreateZone(t *testing.T) {
-	err := models.CreateZone("Zona1", "BlaBla", 1)
-	if err != nil {
-		t.Errorf("err should be null but is %v", err)
-	}
-}
