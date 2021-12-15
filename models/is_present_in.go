1b0d676f005fb2e0ca64bac9c77a3ff544e056b9
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-15 12:43:16 +0100

    Modified is_present_in.go

--- models/is_present_in.go
+++ models/is_present_in.go
@@ -9,8 +9,3 @@ type IsPresentIn struct {
 func (IsPresentIn) TableName() string {
 	return "is_present_in"
 }
-
-func AddObjectToPath(pathId, objectId, order uint) error {
-	tx := Db.Create(&IsPresentIn{PathID: pathId, ObjectID: objectId, Order: order})
-	return tx.Error
-}
