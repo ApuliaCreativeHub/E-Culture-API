8ed05568f024afde2e138fbd6495d853edb33db6
BrattaFrancesco <fra.bra00@gmail.com>
2021-12-16 12:25:20 +0100

    Starting webserver

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module E-Culture-API
 go 1.17
 
 require (
+	github.com/gorilla/mux v1.8.0
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.2.1
 	gorm.io/gorm v1.22.4
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"E-Culture-API/router"
+	"E-Culture-API/utils"
+	"log"
+	"net/http"
+	"time"
+)
 
+var configPath = "conf/conf.yml"
+
+func main() {
+	conf, err := utils.GetConfig(configPath)
+	if err != nil {
+		return
+	}
+	srv := &http.Server{
+		Handler:      router.Router(),
+		Addr:         conf.Server.Host + ":" + conf.Server.Port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
