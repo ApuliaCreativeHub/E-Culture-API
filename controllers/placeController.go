ad6c47b5f50b149ac87d6b1266317cdcefbf246f
DarkFighterLuke <luca-consoli@live.it>
2022-01-24 19:15:40 +0100

    Implemented place update function
    Signed-off-by: DarkFighterLuke <luca-consoli@live.it>

--- controllers/placeController.go
+++ controllers/placeController.go
@@ -5,6 +5,7 @@ import (
 	"E-Culture-API/models"
 	"encoding/json"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"strconv"
 )
@@ -12,49 +13,9 @@ import (
 //AddPlace handles endpoint place/add
 func AddPlace(w http.ResponseWriter, r *http.Request) {
 	if checkAuthorization(r) {
-		user, err := getUserByToken(r)
-		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		place := new(models.Place)
-		place.UserID = user.ID
+		place, photo := retrieveMultipartPlace(w, r)
 
-		err = r.ParseMultipartForm(0)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte(utils.MalformedData))
-			return
-		}
-		place.Name = r.PostFormValue("name")
-		place.Address = r.PostFormValue("address")
-		place.Description = r.PostFormValue("description")
-
-		tempPlace := models.Place{Address: place.Address}
-		err = tempPlace.ReadByAddress()
-		if tempPlace.ID != 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte(utils.PlaceAddressAlreadyExists))
-			return
-		}
-		var ll utils.LatLong
-		err = utils.RetrieveLatLong(place.Address, &ll)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte(utils.AddressSearchFailed))
-			return
-		}
-		place.Lat = ll.Lat
-		place.Long = ll.Long
-
-		photo, _, err := r.FormFile("img")
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte(utils.ProcessingImagesFailed))
-			return
-		}
-
-		err = place.Create()
+		err := place.Create()
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte(utils.General5xx))
@@ -80,6 +41,52 @@ func AddPlace(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
 	}
+}
+
+func retrieveMultipartPlace(w http.ResponseWriter, r *http.Request) (*models.Place, multipart.File) {
+	user, err := getUserByToken(r)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return nil, nil
+	}
+	place := new(models.Place)
+	place.UserID = user.ID
+
+	err = r.ParseMultipartForm(0)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(utils.MalformedData))
+		return nil, nil
+	}
+	place.Name = r.PostFormValue("name")
+	place.Address = r.PostFormValue("address")
+	place.Description = r.PostFormValue("description")
+
+	tempPlace := models.Place{Address: place.Address}
+	err = tempPlace.ReadByAddress()
+	if tempPlace.ID != 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(utils.PlaceAddressAlreadyExists))
+		return nil, nil
+	}
+	var ll utils.LatLong
+	err = utils.RetrieveLatLong(place.Address, &ll)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(utils.AddressSearchFailed))
+		return nil, nil
+	}
+	place.Lat = ll.Lat
+	place.Long = ll.Long
+
+	photo, _, err := r.FormFile("img")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(utils.ProcessingImagesFailed))
+		return nil, nil
+	}
+
+	return place, photo
 }
 
 //GetYourPlaces handles endpoint place/getYours
@@ -167,6 +174,31 @@ func DeletePlace(w http.ResponseWriter, r *http.Request) {
 		}
 
 		err = place.Delete()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(utils.General5xx))
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+// UpdatePlace handles endpoint place/update
+func UpdatePlace(w http.ResponseWriter, r *http.Request) {
+	if checkAuthorization(r) {
+		place, photo := retrieveMultipartPlace(w, r)
+
+		path := "static/images/" + strconv.Itoa(int(place.ID))
+		err := utils.MakeImgs(photo, path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(utils.General5xx))
+			return
+		}
+
+		place.PhotoPath = path
+		err = place.Update()
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte(utils.General5xx))
